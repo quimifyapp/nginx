845cd156523f773e767ebf83a7e9597fad346775
Quimify <contacto@quimify.com>
2023-11-20 00:58:58 +0100

    Add support for Cloudflare IP V6

--- scripts/update-cloudflare-ips.sh
+++ scripts/update-cloudflare-ips.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-CLOUDFLARE_IPS_URL=https://www.cloudflare.com/ips-v4/
 CLOUDFLARE_IPS_ONLY_FILE=/etc/nginx/cloudflare-ips-only.conf
-CLOUDFLARE_IP_REGEX="^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}$"
 
-# Download IP list
-cloudflare_ips=$(curl -s "$CLOUDFLARE_IPS_URL")
+CLOUDFLARE_IPS_V4_URL=https://www.cloudflare.com/ips-v4
+CLOUDFLARE_IPS_V6_URL=https://www.cloudflare.com/ips-v6
+
+CLOUDFLARE_IP_V4_REGEX="^([0-9]{1,3}\.){3}[0-9]{1,3}/[0-9]{1,2}$"
+CLOUDFLARE_IP_V6_REGEX="^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))\/[0-9]{1,2}$"
+
+# Download IP V4 list
+cloudflare_ips_v4=$(curl -s "$CLOUDFLARE_IPS_V4_URL")
+
+echo "$cloudflare_ips_v4"
 
 # Check content
-if [ -z "$cloudflare_ips" ]
+if [ -z "$cloudflare_ips_v4" ]
 then
 	echo
-	echo "Fetched an empty response from URL: "$CLOUDFLARE_IPS_URL""
+	echo "Fetched an empty response from URL: '"$CLOUDFLARE_IPS_URL"'"
 	echo "Didn't update Cloudflare IPs"
 	echo
 	exit 1
@@ -19,16 +25,41 @@ fi
 
 # Check each line individually
 while IFS= read -r line; do
-	if ! [[ $line =~ $CLOUDFLARE_IP_REGEX ]]; then
+	if ! [[ $line =~ $CLOUDFLARE_IP_V4_REGEX ]]; then
 		echo
-		echo "Invalid Cloudflare IP detected in line: "$line""
+		echo "Invalid Cloudflare IP V4 detected in line: '"$line"'"
 		echo "Didn't update Cloudflare IPs"
 		echo
 		exit 1
 	fi
-done <<< "$cloudflare_ips"
+done <<< "$cloudflare_ips_v4"
+
+# Download IP V6 list
+cloudflare_ips_v6=$(curl -s "$CLOUDFLARE_IPS_V6_URL")
+
+# Check content
+if [ -z "$cloudflare_ips_v6" ]
+then
+	echo
+	echo "Fetched an empty response from URL: '"$CLOUDFLARE_IPS_URL"'"
+	echo "Didn't update Cloudflare IPs"
+	echo
+	exit 1
+fi
+
+# Check each line individually
+while IFS= read -r line; do
+	if ! [[ $line =~ $CLOUDFLARE_IP_V6_REGEX ]]; then
+		echo
+		echo "Invalid Cloudflare IP V6 detected in line: '"$line"'"
+		echo "Didn't update Cloudflare IPs"
+		echo
+		exit 1
+	fi
+done <<< "$cloudflare_ips_v6"
 
 # Update file
+cloudflare_ips="$cloudflare_ips_v4"$'\n'"$cloudflare_ips_v6"
 (echo "$cloudflare_ips" | sed "s|^|allow |g" | sed "s|\$|;|g" && echo "deny all;") > "$CLOUDFLARE_IPS_ONLY_FILE"
 
 # Show file
